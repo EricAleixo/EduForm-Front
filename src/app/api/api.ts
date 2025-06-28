8f1b3ae489935ed17389b39ef338dce730605b53
ericAleixo <ericaleixo66@gmail.com>
2025-06-28 17:38:18 -0300

    ADD: corrigi o path da apiUrl

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -4,5 +4,5 @@ import { StudentFormData } from "../types/formData.types";
 const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/students";
 
 export const postStudents = async (formData: StudentFormData) => {
-    return await axios.post(apiUrl, formData);
+    return await axios.post(apiUrl + "/students", formData);
 }
